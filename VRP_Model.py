e9aaa8d79ba338e6178c4b75d8abc259ea72d3b7
ElenaTzerefou <56511251+ElenaTzerefou@users.noreply.github.com>
2021-01-05 19:37:56 +0200

    Update VRP_Model.py

--- VRP_Model.py
+++ VRP_Model.py
@@ -10,37 +10,40 @@ class Model:
         self.distance = []
         self.time = []
         self.capacity = -1
+        self.numberOf1500 = 15
+        self.numberOf1200 = 15
 
     def BuildModel(self):
         # birthday 08/02/1999
         birthday = 8021999
         random.seed(birthday)
-        all_nodes = []
-        customers = []
+        self.allNodes = []
+        self.customers = []
         depot = Node(0, 0, 0, 50, 50)
-        all_nodes.append(depot)
+        self.allNodes.append(depot)
         random.seed(1)
         for i in range(0, 100):
             id = i + 1
-        dem = random.randint(1, 5) * 100
-        xx = random.randint(0, 100)
-        yy = random.randint(0, 100)
-        st = 0.25  # 15 minutes in hrs
-        cust = Node(id, st, dem, xx, yy)
-        all_nodes.append(cust)
-        customers.append(cust)
+            dem = random.randint(1, 5) * 100
+            xx = random.randint(0, 100)
+            yy = random.randint(0, 100)
+            st = 0.25  # 15 minutes in hrs
+            cust = Node(id, st, dem, xx, yy)
+            self.allNodes.append(cust)
+            self.customers.append(cust)
 
-        rows = len(self.allNodes)
-        self.distance = [[0.0 for x in range(rows)] for y in range(rows)]
-        self.time = [[0.0 for x in range(rows)] for y in range(rows)]
+            rows = len(self.allNodes)
+            self.distance = [[0.0 for x in range(rows)] for y in range(rows)]
+            self.time = [[0.0 for x in range(rows)] for y in range(rows)]
 
-        for i in range(0, len(self.allNodes)):
-            for j in range(0, len(self.allNodes)):
-                a = self.allNodes[i]
-                b = self.allNodes[j]
-                dist = math.sqrt(math.pow(a.x - b.x, 2) + math.pow(a.y - b.y, 2))
-                self.distance[i][j] = dist
-                self.time[i][j] = dist / 35 + 0.25
+            for i in range(0, len(self.allNodes)):
+                for j in range(0, len(self.allNodes)):
+                    a = self.allNodes[i]
+                    b = self.allNodes[j]
+                    dist = math.sqrt(math.pow(a.x - b.x, 2) + math.pow(a.y - b.y, 2))
+                    self.distance[i][j] = dist
+                    self.time[i][j] = dist / 35 + 0.25
+
 
 class Node:
     def __init__(self, id, st, dem, xx, yy):
@@ -52,10 +55,8 @@ class Node:
         self.isRouted = False
 
 class Route:
-    def __init__(self, dp, cap):
+    def __init__(self, cap):
         self.sequenceOfNodes = []
-        ##self.sequenceOfNodes.append(dp)
-        ##self.sequenceOfNodes.append(dp)
         self.capacity = cap
         self.load = 0
         self.maxtime = 3.5
