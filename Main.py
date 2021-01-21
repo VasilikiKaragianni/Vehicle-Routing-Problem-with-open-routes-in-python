08d364f3d5d0f498993dc55cca1708bcab3a9f92
ElenaTzerefou <56511251+ElenaTzerefou@users.noreply.github.com>
2021-01-21 18:30:40 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,30 @@
 from Solver import *
 
-m = Model ()
+m = Model()
 m.BuildModel()
 s = Solver(m)
 solution = s.ApplyNearestNeighborMethod()
 print(s.objective(solution))
-solution2 = s.LocalSearch()
-print(s.objective(solution2))
-# solution3 = s.VND()
-# print(s.objective(solution3))
+for rout in solution.routes:
+    sdem = 0
+    for nod in rout.sequenceOfNodes:
+        sdem += nod.demand
+    print('sdem- ' + str(sdem))
+    print('rout.load- ' + str(rout.load))
+# solution2 = s.LocalSearch()
+# print(s.objective(solution2))
+# for rout in solution2.routes:
+#     sdem = 0
+#     for nod in rout.sequenceOfNodes:
+#         sdem += nod.demand
+#     print('sdem- ' + str(sdem))
+#     print('rout.load- ' + str(rout.load))
+solution3 = s.VND()
+print(s.objective(solution3))
+for rout in solution3.routes:
+    sdem = 0
+    for nod in rout.sequenceOfNodes:
+        sdem += nod.demand
+    print('sdem- ' + str(sdem))
+    print('rout.load- ' + str(rout.load))
+
