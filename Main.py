4cf15045ac9fcb1be76f34f9f4f3e3f784638dd8
ElenaTzerefou <56511251+ElenaTzerefou@users.noreply.github.com>
2021-01-22 10:40:54 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,12 +5,12 @@ m.BuildModel()
 s = Solver(m)
 solution = s.ApplyNearestNeighborMethod()
 print(s.objective(solution))
-for rout in solution.routes:
-    sdem = 0
-    for nod in rout.sequenceOfNodes:
-        sdem += nod.demand
-    print('sdem- ' + str(sdem))
-    print('rout.load- ' + str(rout.load))
+# for rout in solution.routes:
+#     sdem = 0
+#     for nod in rout.sequenceOfNodes:
+#         sdem += nod.demand
+#     print('sdem- ' + str(sdem))
+#     print('rout.load- ' + str(rout.load))
 # solution2 = s.LocalSearch()
 # print(s.objective(solution2))
 # for rout in solution2.routes:
@@ -21,10 +21,9 @@ for rout in solution.routes:
 #     print('rout.load- ' + str(rout.load))
 solution3 = s.VND()
 print(s.objective(solution3))
-for rout in solution3.routes:
-    sdem = 0
-    for nod in rout.sequenceOfNodes:
-        sdem += nod.demand
-    print('sdem- ' + str(sdem))
-    print('rout.load- ' + str(rout.load))
-
+# for rout in solution3.routes:
+#     sdem = 0
+#     for nod in rout.sequenceOfNodes:
+#         sdem += nod.demand
+#     print('sdem- ' + str(sdem))
+#     print('rout.load- ' + str(rout.load))
